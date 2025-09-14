0c75483438d252c3bf02e5991b233bbb54b3c176
KevinKagwima <KevinKagwima+github1@gmail.com>
2025-09-14 23:16:06 +0300

    updated files

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -60,10 +60,12 @@ function executeWork(employee: Teacher | Director): string {
 
 type Subjects = "Math" | "History";
 
-function todayClass(todayClass: Subjects): string {
+function teachClass(todayClass: Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
   } else if (todayClass === "History") {
     return "Teaching History";
   }
 }
+
+console.log(teachClass("Math"));
