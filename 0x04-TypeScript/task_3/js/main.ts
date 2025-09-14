9ad18f0385b3da80f2121710a975e0192639b41e
KevinKagwima <KevinKagwima+github1@gmail.com>
2025-09-14 23:29:49 +0300

    updated files

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -7,7 +7,7 @@ const row: RowElement = {
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
-UpdatedRow: RowElement;
+UpdatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, UpdatedRow);
 CRUD.deleteRow(newRowID);
